36157b9170d805cef0caa01263d5349d0ba4f4d8
Christopher Francisco <francistopherr@icloud.com>
2021-02-27 23:50:08 -0500

    CREATE Project

---
+++ CHAPTER_01/display_sales_tax.php
@@ -0,0 +1,67 @@
+<?php
+// get the data from the form
+$product_description = $_POST['product_description'];
+$list_price = $_POST['list_price'];
+$tax_percent = $_POST['tax_percent'];
+
+// Catch a non numeric list_price
+if (!number_format($list_price, 2)) {
+    $list_price = 0.0;
+}
+
+// Catch a non numeric discount_percent
+if (!number_format($tax_percent, 2)) {
+    $tax_price = 0.0;
+}
+
+// calculate the discount
+$tax = $list_price * $tax_percent * .01;
+$total_price = $list_price + $tax;
+
+// apply currency formatting to the dollar and percent amounts
+$list_price_formatted = "$".number_format($list_price, 2);
+$tax_percent_formatted = number_format($tax_percent, 1)."%";
+$tax_formatted = "$".number_format($tax, 2);
+$total_price_formatted = "$".number_format($total_price, 2);
+
+// escape the unformatted input
+if ($product_description == "") {
+    $product_description = "Unknown Product";
+}
+$product_description_escaped = htmlspecialchars($product_description);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <meta name="viewport" content="width=device-width, initial-width=1, minimum-scale=1, user-scalable=no"/>
+</head>
+
+<body>
+
+<main>
+        <h1>Product Tax Calculator</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo $product_description_escaped; ?></span><br>
+
+        <label>List Price:</label>
+        <span><?php echo $list_price_formatted; ?></span><br>
+
+        <label>Standard Sales Tax:</label>
+        <span><?php echo $tax_percent_formatted; ?></span><br>
+
+        <label>Sales Tax Amount:</label>
+        <span><?php echo $tax_formatted; ?></span><br>
+
+        <label>Total Price:</label>
+        <span><?php echo $total_price_formatted; ?></span><br>
+    </main>
+
+</body>
+    </html>
+
---
+++ README.md
@@ -0,0 +1,17 @@
+# My PHP Playground
+A repository illustrating my tinkering and progress of learning [PHP](https://www.php.net/) because it is open source 
+and [WordPress](https://wordpress.com/) uses it. Going to use online resources and a book most likely.
+# Contents :meat_on_bone:
+
+| Source Code | Project | Unbreakable |
+| ----------- | ------- | ----------- |
+| [Chapter 1](CHAPTER_01) | [Product Sales Tax Calculator](https://youtu.be/d054CRkQeQM) | :x: |
+
+# Execution :gear:
+At first I was surprised that nothing showed up after I opened the html file containing PHP. I learned that JavaScript is automatically served by the browser, not PHP. A server must be created to serve the PHP code.
+
+"bUt I dOnT hAvE a SeRvEr!"
+
+Not to worry, follow a [tutorial](https://www.youtube.com/watch?v=RwQW0Qy85jY) to run the PHP on a website. Use open source [software](https://www.mamp.info/en/windows/) to serve your PHP code as highlighted in the tutorial. If you can't figure it out, I don't know what to tell you. You might have an amoeba for a brain or something.
+
+Instead of searching for index.html, the server searches for index.php
